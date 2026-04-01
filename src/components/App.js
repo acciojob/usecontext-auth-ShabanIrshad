c6dd061c04e3856dc766ca1975bf9be9e8a67f1f
Shaban Irshad <shabanirshad408@gmail.com>
2026-04-01 11:36:53 +0530

    added

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
 
-import React from "react";
+import React, { createContext, useContext, useState } from "react";
 import './../styles/App.css';
+ const AuthContext=createContext();
 
 const App = () => {
+  const [auth,setAuth]=useState(false);
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <AuthContext.Provider value={auth}>
+      <div>
+          <h2>Click on the checkbox to get Authenticated.</h2>
+         
+          <Auth/> <br/><br/>
+          <input id="checkbox" type="checkbox" onChange={()=>setAuth(!auth)}/><label htmlFor="checkbox">I'm not a robot</label>
+          
+      </div>
+    </AuthContext.Provider>
   )
+}
+
+function Auth(){
+  const auth=useContext(AuthContext);
+  console.log(auth);
+  return(
+    <>
+      {auth?'You are now authenticated,you can proceed.':'You are not authenticated.'}
+    </>
+  );
 }
 
 export default App
